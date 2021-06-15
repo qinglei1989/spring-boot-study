83f0da73a684882a8a76d33dd6867e5c3b7937ae
王清雷 <wangqinglei01@renrenche.com>
2021-06-15 22:10:07 +0800

    feat:commit提交回滚

---
+++ src/main/java/com/rrc/constant/CommonConst.java
@@ -0,0 +1,20 @@
+package com.rrc.constant;
+
+/**
+ * @ClassName CommonConst
+ * @Description TODO
+ * @Author wang
+ * @Date 2021/6/14 15:57
+ * @Version 1.0
+ **/
+public class CommonConst {
+    /**
+     * 已删除
+     **/
+    public static final String DEL_FLAG_0 = "0";
+
+    /**
+     * 未删除
+     **/
+    public static final String DEL_FLAG_1 = "1";
+}
